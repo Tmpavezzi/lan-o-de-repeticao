951ec95560903a60ae7f899b54cbe744c9ca190f
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-28 22:15:03 -0300

    Create Quadrado do numero.java

---
+++ Quadrado do numero.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);
+int n1 ;
+
+
+    for (int i=0;i<5;i++){
+          System.out.print("Digite um numero ");
+          n1 = entrada.nextInt();
+      
+          System.out.println("O quadrado do numero "+ n1 + " e " + (n1*n1) + "\n");
+    }
+
+
+
+
+
+    
+    
+    
+     
+    
+    
+    
+    }
+
+    
+  }
