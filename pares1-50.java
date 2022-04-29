e9b50962b849ecb38a34ddb1960488183584a4ef
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-28 21:47:01 -0300

    Create pares1-50.java

---
+++ pares1-50.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+       int n=1;
+   
+    
+      for( n = 2;n<50;n=n+2){
+        System.out.println(n);
+        
+        
+      }
+    }
+
+    
+  }
