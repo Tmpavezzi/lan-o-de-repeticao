ac73e1af1975054fa823360a7e8dab52512a7bec
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-28 22:28:10 -0300

    Create tabuada.java

---
+++ tabuada.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);
+int n1 ;
+System.out.print("Digite um numero ");
+    n1= entrada.nextInt();
+for (int i=1;i<10;i++){
+  //n x 1 =n*1
+    //n x 2 =n*2
+  
+      System.out.println( n1 +" x "+ i  + " = "+(n1*i) +"\n");
+      
+}
+    
+    
+
+
+    
+    }
+
+    
+  }
